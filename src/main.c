6a97a952b899475e7f61fb4c7489db60dcf52e40
avinashmuthoju <avinashmuthoju@gmail.com>
2026-08-19 16:10:22 +0530

    Break down to .c & .h files

--- src/main.c
+++ src/main.c
@@ -1,25 +1,11 @@
 #include<stdio.h>
 #include<unistd.h>
-#include<string.h>
 #include<stdlib.h>
+#include "user_path.h"
 
 char curr_dir[1024];
 char host_name[1024];
 char home_dir[1024];
-
-void get_path(char* path){
-    int home_len=strlen(home_dir);
-    if(strcmp(home_dir,curr_dir)==0){
-        strcpy(path,"~");
-    }
-    else if(strncmp(home_dir,curr_dir,home_len)==0 && curr_dir[home_len]=='/'){
-        strcpy(path,"~");
-        strcat(path,curr_dir+home_len);
-    }
-    else{
-        strcpy(path,curr_dir);
-    }
-}
 
 int main(){
     
@@ -32,7 +18,7 @@ int main(){
         
         getcwd(curr_dir,sizeof(curr_dir));
 
-        get_path(path);
+        get_path(path,home_dir,curr_dir);
         printf("<%s@%s:%s>",user_name,host_name,path);
         scanf("%s",input);
     }    
